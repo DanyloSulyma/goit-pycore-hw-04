a88b0af462ace56b83eb0854ecda0ed53dbacfad
DANYLO SULYMA <Danulo007@gmail.com>
2026-02-16 13:17:40 +0100

    change

--- Task_4.py
+++ Task_4.py
@@ -1,25 +1,75 @@
 def parse_input(user_input):
+    if not user_input.strip():
+        return "empty", []
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
 def add_contact(args, contacts):
+    if len(args) < 2:
+        return "Error: Give me name and phone please."
     name, phone = args
     contacts[name] = phone
     return "Contact added"
+
+def change_contact(args, contacts):
+    if len(args) < 2:
+        return "Error: Give me name and phone please."
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return "Error: Contact not found."
+
+def show_phone(args, contacts):
+    if len(args) < 1:
+        return "Error: Enter user name."
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "Error: Contact not found."
+
+def show_all(contacts):
+    if not contacts:
+        return "Contact list is empty."
+    result = ""
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+    return result.strip()
 
 
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
+    
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
+
         if command in ["close", "exit"]:
             print("Good bye!")
             break
+            
         elif command == "hello":
-            print("How can i help you?")
+            print("How can I help you?")
+            
+        elif command == "add":
+            print(add_contact(args, contacts))
+            
+        elif command == "change":
+            print(change_contact(args, contacts))
+            
+        elif command == "phone":
+            print(show_phone(args, contacts))
+            
+        elif command == "all":
+            print(show_all(contacts))
+            
+        elif command == "empty":
+            continue
+            
         else:
             print("Invalid command.")
 
